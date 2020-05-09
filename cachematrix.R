5bf0a5f0fca8410defa6f00a4db5bfa70062151d
He-commitsAA <60632165+He-commitsAA@users.noreply.github.com>
2020-05-09 18:52:34 -0500

    Solved the exercise

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions create and invert matrices, returning the inverse
+## if the inverse has already been calculated.
 
-## Write a short comment describing this function
+## Makes matrices and can cache the inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+                i <- NULL
+                setVals <- function(y){
+                        x <<- y
+                        i <<- NULL
+                }
+                getVals <- function() x
+                setInv <- function(inv){
+                        i <<- inv
+                }
+                getInv <- function() i
+                list(setVals = setVals, getVals = getVals, setInv = setInv,
+                     getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## Calculates or returns the inverse of a matrix depending on if 
+## the inverse has already been calculated.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        i <- x$getInv()
+        if(!is.null(i)){
+                message("getting cached inverse...")
+                return(i)
+        }
+        data <- x$getVals()
+        i <- solve(data,...)
+        x$setInv(i)
+        i
 }
